f1ac5081d8d79fd2dc2671e53312803a5df3b1ba
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2025-07-11 10:42:33 +0530

    Add files via upload

---
+++ EvenNo.java
@@ -0,0 +1,25 @@
+/*Author:Divya Kuriakose
+  Date:11-07-2025
+  Program in java to print the even numbers in a limit
+ */
+
+package mynewproject;
+
+import java.util.Scanner;
+
+public class EvenNo {
+		public static void main(String[] args) {
+	    	Scanner scanner = new Scanner(System.in);
+	    	System.out.println("Enter the limit");
+	    	int limit = scanner.nextInt();
+	    	int evenNo;
+	    	for(int i=2;i<limit;i+=2){
+	
+	    		   System.out.println(i + "\t");
+	    	   }
+	    	   
+	    	}
+	    	
+	    		
+	    	}
+
