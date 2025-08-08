9bb3471cd94f5defcd0121257fbe6d268925cc54
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2025-08-08 10:36:29 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,43 @@
-package demo;
+package test;
 
-class Student{
-	String name;
-	int rollNumber;
-	static int studentCount=0;
-	Student(String name,int rollNUmber){
-		this.name=name;
-		this.rollNumber=rollNumber;
-		studentCount++;
+class Vehicle{
+	String brand;
+	String model;
+	
+	Vehicle(String brand,String model){
+		this.brand=brand;
+		this.model=model;
 	}
-	public static void displayStudentCount() {
-		System.out.println("Total number of student is: "+studentCount);
+	public void displayInfo() {
+		System.out.println("Brand name: "+brand);
+		System.out.println("Model: "+model);
 	}
 }
-
-public class Main {
-	public static void main(String[] args) {
-		Student student1=new Student("Alice",12);
-		Student student2=new Student("Anu",14);
-		Student student3=new Student("Aparna",18);
-		
-		Student.displayStudentCount();
+class Car extends Vehicle{
+	String fuelType;
+	Car(String brand,String model,String fuelType){
+		super(brand,model);
+		this.fuelType=fuelType;
 	}
-
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("Fuel Type: "+fuelType);
+	}
+}
+class ElectricCar extends Car{
+	double batteryCapacity;
+	ElectricCar(String brand,String model,String fuelType,double batteryCapacity){
+		super(brand,model,fuelType);
+		this.batteryCapacity=batteryCapacity;
+	}
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("Battery Capacity: "+batteryCapacity);
+	}
+}
+public class Main {
+   public static void main(String[] args) {
+	   ElectricCar ecar=new ElectricCar("Tesla","Model S","Electric",100);
+       ecar.displayInfo();
+   }
 }
