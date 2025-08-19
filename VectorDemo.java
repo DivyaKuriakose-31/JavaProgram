33c2c169bacf3d3476f13fba1b84240243bc9159
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2025-08-19 15:22:39 +0530

    Add files via upload

---
+++ VectorDemo.java
@@ -0,0 +1,15 @@
+package trial;
+import java.util.Scanner;
+public class VectorDemo {
+	public static void main(String[] args) {
+		Vector <String>mammals=new Vector<>();
+		Vector <Integer>rollNos=new Vector<>();
+		rollNos.add(1);
+		rollNos.add(2);
+		System.out.println("Roll Nos: "+rollNos);
+		rollNos.remove(0);
+		
+		System.out.println("Roll Nos: "+rollNos);
+		rollNos.remove(2);
+	}
+}
